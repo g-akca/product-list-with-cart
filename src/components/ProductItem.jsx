be8a876df86652fa71432a0d8a0249c3ad60a6e9
g-akca <gok.akca66@gmail.com>
2026-03-12 02:53:04 +0300

    Add ProductList and ProductItem structure and styles

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,22 @@
+import productImg from "/assets/images/image-waffle-mobile.jpg";
+import addToCartIcon from "/assets/images/icon-add-to-cart.svg";
+import "./ProductItem.css";
+
 function ProductItem() {
   return (
     <section className="product-item">
+      <img src={productImg} className="product-img" />
+
+      <button type="button" className="add-to-cart-btn">
+        <img src={addToCartIcon} />
+        <p>Add to Cart</p>
+      </button>
+
+      <div className="product-details">
+        <p className="category">Waffle</p>
+        <h3 className="name">Waffle with Berries</h3>
+        <p className="price">$6.50</p>
+      </div>
     </section>
   )
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,5 @@
 import ProductItem from "./ProductItem";
+import "./ProductList.css";
 
 function ProductList() {
   return (
