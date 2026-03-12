f5722fa27550796ddca70a16617c7cd4142e3c03
g-akca <gok.akca66@gmail.com>
2026-03-12 09:00:32 +0300

    Add hover styles

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,4 +1,3 @@
-import removeItemIcon from "/assets/images/icon-remove-item.svg";
 import "./CartItem.css";
 
 function CartItem({ name, quantity, price, removeFromCart }) {
@@ -15,7 +14,9 @@ function CartItem({ name, quantity, price, removeFromCart }) {
       </div>
 
       <button type="button" className="remove-btn" onClick={removeFromCart}>
-        <img src={removeItemIcon} />
+        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+          <path d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/>
+        </svg>
       </button>
     </div>
   )
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,4 @@
 import addToCartIcon from "/assets/images/icon-add-to-cart.svg";
-import decrementIcon from "/assets/images/icon-decrement-quantity.svg";
-import incrementIcon from "/assets/images/icon-increment-quantity.svg";
 import "./ProductItem.css";
 
 function ProductItem({ image, name, category, price, quantity, addToCart, removeFromCart }) {
@@ -20,11 +18,15 @@ function ProductItem({ image, name, category, price, quantity, addToCart, remove
       ) : (
         <div className="cart-controls">
           <button type="button" className="control-btn" onClick={removeFromCart}>
-            <img src={decrementIcon} />
+            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
+              <path d="M0 .375h10v1.25H0V.375Z"/>
+            </svg>
           </button>
           <span>{quantity}</span>
           <button type="button" className="control-btn" onClick={addToCart}>
-            <img src={incrementIcon} />
+            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+              <path d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+            </svg>
           </button>
         </div>
       )}
