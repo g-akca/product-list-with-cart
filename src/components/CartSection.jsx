5defb0086cdc0b108e8b0868af24cd86770e4ebd
g-akca <gok.akca66@gmail.com>
2026-03-12 03:01:33 +0300

    Add static CartSection styles

--- src/components/CartSection.jsx
+++ src/components/CartSection.jsx
@@ -1,7 +1,15 @@
+import emptyCartIcon from "/assets/images/illustration-empty-cart.svg";
+import "./CartSection.css";
+
 function CartSection() {
   return (
     <section className="cart-section">
       <h2>Your Cart (0)</h2>
+
+      <div className="cart-main">
+        <img src={emptyCartIcon} className="empty-cart-img" />
+        <p>Your added items will appear here</p>
+      </div>
     </section>
   )
 }
